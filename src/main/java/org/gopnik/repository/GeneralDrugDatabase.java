4f818ff033a8e88852d49af1c1883cff24d56c5a
zouuek <zouu3k@gmail.com>
2024-10-24 13:43:49 +0200

    komit typu nic sie nie zmienilo w razie gdyby ktos chcial cos kodowac bo ja tera czasu nei amm (important preparatory changes for first major update)

--- src/main/java/org/gopnik/repository/GeneralDrugDatabase.java
+++ src/main/java/org/gopnik/repository/GeneralDrugDatabase.java
@@ -1,16 +1,40 @@
 package org.gopnik.repository;
 
 import jakarta.persistence.EntityManager;
+import jakarta.persistence.NoResultException;
 import jakarta.persistence.PersistenceContext;
+import jakarta.persistence.TypedQuery;
+import org.gopnik.model.Drug;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
 
 @Repository
 public class GeneralDrugDatabase implements IGeneralDrugDatabase{
 
     @PersistenceContext
     private EntityManager entityManager;
+    private final String GET_BY_NAME= "SELECT i FROM com.gopnik.model.Drug i WHERE i.name =':name'";
+    private final String GET_ALL="FROM com.gopnik.Drug";
     public GeneralDrugDatabase(EntityManager entityManager){
         this.entityManager = entityManager;
+    }
+    @Override
+    public Optional<Drug> getByName(String name) {
+        TypedQuery<Drug> query = entityManager.createQuery(GET_BY_NAME, Drug.class);
+        query.setParameter("name", name);
+        try {
+            return Optional.of(query.getSingleResult());
+        } catch (NoResultException e) {
+            return Optional.empty();
+        }
+    }
+    @Override
+    public List<Drug> getAll() {
+        TypedQuery<Drug> query = entityManager.createQuery(GET_ALL, Drug.class);
+        List<Drug> result = query.getResultList();
+        return result;
     }
 
 
--- src/main/java/org/gopnik/repository/IGeneralDrugDatabase.java
+++ src/main/java/org/gopnik/repository/IGeneralDrugDatabase.java
@@ -1,4 +1,11 @@
 package org.gopnik.repository;
 
+import org.gopnik.model.Drug;
+
+import java.util.List;
+import java.util.Optional;
+
 public interface IGeneralDrugDatabase {
+    public Optional<Drug> getByName(String name);
+    public List<Drug> getAll();
 }
