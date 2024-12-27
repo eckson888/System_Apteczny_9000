42db39216ace630ec1a9eb610839e914bda117b6
Kacper Dęga <kacperdgf@o2.pl>
2024-12-27 21:12:37 +0100

    zajebisty front eventlog

---
+++ src/main/resources/static/js/eventlogs.js
@@ -0,0 +1,34 @@
+function reloadLogs() {
+    const date = document.getElementById("datePicker").value;
+
+    let logsContainer = document.getElementById("logsContainer");
+
+    let link = "http://localhost:8080/logs/" + date;
+
+    fetch(link)
+        .then(res => res.json())
+        .then((resArray) => {
+        if (Array.isArray(resArray)) {
+            if (resArray.length == 0) {
+                logsContainer.innerHTML = "\n<h2> NO LOGS AT " + date + "</h2>";
+                return;
+            }
+
+            logsContainer.innerHTML = "";
+
+            logsContainer.innerHTML +=
+                        `<div class="download-buttons">
+                            <a href="/logs/download/csv/2024-12-26">Pobierz jako CSV</a>
+                            <a href="/logs/download/txt/2024-12-26">Pobierz jako TXT</a>
+                        </div>`;
+
+            logsContainer.innerHTML += `\n<textarea disabled>${resArray.join("\n")}</textarea>`;
+        } else {
+            logsContainer.innerHTMl = "\n<p>INVALID RESPONSE</p>";
+        }
+    })
+        .catch(err => {
+        logsContainer.innerHTMl = "\n<p>ERROR</p>";
+        console.error("Error fetching data: ", err);
+    });
+}
